7f250d0bb1884c3e71577b65be57828d81956290
FinityM <B00125089@mytudublin.ie>
2021-12-06 14:19:09 +0000

    Deleted files from lab 4

--- Lab5/parallelmean_from_lab4/functions.h
+++
@@ -1,9 +0,0 @@
-void *child_hello(void* rank); // Thread function 
-
-// Declare “Child” Thread function
-void *mythread_func(void *param);
-
-// Global thread count variable
-int thread_count;
-
-
--- Lab5/parallelmean_from_lab4/sum_func.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <pthread.h>
-
-float sum_func(void* p){
-    
-
-}
